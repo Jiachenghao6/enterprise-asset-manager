a78aa416ae705d187492c96f15d451cd719dd190
jj <jiachenghao0411@gmail.com>
2026-01-18 15:42:51 +0100

    feat: initial project setup with postgres configuration

---
+++ src/main/java/du/tum/student/enterpriseassetmanager/EnterpriseAssetManagerApplication.java
@@ -0,0 +1,13 @@
+package du.tum.student.enterpriseassetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAssetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAssetManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/du/tum/student/enterpriseassetmanager/EnterpriseAssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package du.tum.student.enterpriseassetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAssetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
