d3e2d920535e670de5b65a0320887bf9bb62b75d
Jiachenghao6 <jiachenghao0411@gmail.com>
2026-01-22 22:45:08 +0100

    IMPL Service

--- src/main/java/du/tum/student/enterpriseassetmanager/repository/AssetRepository.java
+++ src/main/java/du/tum/student/enterpriseassetmanager/repository/AssetRepository.java
@@ -1,16 +1,18 @@
 package du.tum.student.enterpriseassetmanager.repository;
 
 import du.tum.student.enterpriseassetmanager.domain.Asset;
+import du.tum.student.enterpriseassetmanager.domain.AssetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
 
-    List<Asset> findByStatus(String status);
+    List<Asset> findByStatus(AssetStatus status);
 
     List<Asset> findByPurchaseDateBetween(LocalDate startDate, LocalDate endDate);
 
@@ -18,9 +20,9 @@ public interface AssetRepository extends JpaRepository<Asset, Long> {
 
     List<Asset> findByPurchaseDateAfter(LocalDate endDate);
 
-    List<Asset> findByPurchaseDateBetweenAndStatus(LocalDate startDate, LocalDate endDate, String status);
+    List<Asset> findByPurchaseDateBetweenAndStatus(LocalDate startDate, LocalDate endDate, AssetStatus status);
 
-    List<Asset> findByPurchasePriceBetween(Double startPrice, Double endPrice);
+    List<Asset> findByPurchasePriceBetween(BigDecimal startPrice, BigDecimal endPrice);
 
-    List<Asset> findByPurchasePriceGreaterThan(Double startPrice);
+    List<Asset> findByPurchasePriceGreaterThan(BigDecimal startPrice);
 }
---
+++ src/main/java/du/tum/student/enterpriseassetmanager/service/AssetService.java
@@ -0,0 +1,29 @@
+package du.tum.student.enterpriseassetmanager.service;
+
+import du.tum.student.enterpriseassetmanager.domain.Asset;
+import du.tum.student.enterpriseassetmanager.repository.AssetRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AssetService {
+    private final AssetRepository assetRepository;
+    private final DepreciationCalculator depreciationCalculator;
+
+    public List<Asset> findAllAssets() {
+        return assetRepository.findAll();
+    }
+
+    public BigDecimal calculateValue(long assetId) {
+       Optional<Asset> asset = assetRepository.findById(assetId);
+       if (asset.isEmpty()) {throw new RuntimeException("Asset not found");}
+       BigDecimal currentValue = depreciationCalculator.calculateCurrentValue(asset.get());
+       return currentValue;
+    }
+
+}
